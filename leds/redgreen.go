67cc3038175bc117fbcd36740b2dffd66f050490
Valentyn Chub <valentin.chub@gmail.com>
2017-09-05 10:29:23 +0300

    add led test and built for brpi

---
+++ leds/redgreen.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"time"
+
+	"periph.io/x/periph/conn/gpio"
+	"periph.io/x/periph/conn/gpio/gpioreg"
+	"periph.io/x/periph/host"
+)
+
+func main() {
+	host.Init()
+	for l := gpio.Low; ; l = !l {
+		gpioreg.ByName("13").Out(l)
+		time.Sleep(500 * time.Millisecond)
+		gpioreg.ByName("6").Out(l)
+		time.Sleep(500 * time.Millisecond)
+	}
+}
