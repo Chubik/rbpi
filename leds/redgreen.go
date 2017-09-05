f195570558cad149de9cea826bcbf385dcc7644d
Valentyn Chub <valentin.chub@gmail.com>
2017-09-05 10:35:32 +0300

    some update tests

--- leds/redgreen.go
+++ leds/redgreen.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"periph.io/x/periph/conn/gpio"
 	"periph.io/x/periph/conn/gpio/gpioreg"
 	"periph.io/x/periph/host"
@@ -8,11 +10,11 @@ import (
 
 func main() {
 	host.Init()
-	// for l := gpio.Low; ; l = !l {
-	// 	gpioreg.ByName("13").Out(l)
-	// 	time.Sleep(500 * time.Millisecond)
-	// }
-	gpioreg.ByName("13").Out(gpio.High)
-	gpioreg.ByName("6").Out(gpio.High)
+	for l := gpio.Low; ; l = !l {
+		gpioreg.ByName("13").Out(l)
+		time.Sleep(500 * time.Millisecond)
+	}
+	// gpioreg.ByName("13").Out(gpio.High)
+	// gpioreg.ByName("6").Out(gpio.High)
 
 }
