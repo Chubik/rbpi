326b7a833356f036fe89a5f5a23da517563c5a32
Valentyn Chub <valentin.chub@gmail.com>
2017-09-05 13:35:04 +0300

    some update tests

--- leds/rgbflat.go
+++ leds/rgbflat.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"sync"
 	"time"
 
 	"periph.io/x/periph/conn/gpio"
@@ -8,15 +10,37 @@ import (
 	"periph.io/x/periph/host"
 )
 
-func main() {
-	host.Init()
-	for l := gpio.Low; ; l = !l {
-		gpioreg.ByName("13").Out(l)
-		time.Sleep(500 * time.Millisecond)
-		gpioreg.ByName("6").Out(l)
-		time.Sleep(500 * time.Millisecond)
-		gpioreg.ByName("5").Out(l)
-		time.Sleep(500 * time.Millisecond)
-	}
+var lights chan string
 
+func main() {
+	lights = make(chan string)
+	var wg sync.WaitGroup
+	host.Init()
+	// for l := gpio.Low; ; l = !l {
+	// 	gpioreg.ByName("13").Out(l)
+	// 	time.Sleep(500 * time.Millisecond)
+	// 	gpioreg.ByName("6").Out(l)
+	// 	time.Sleep(500 * time.Millisecond)
+	// 	gpioreg.ByName("5").Out(l)
+	// 	time.Sleep(500 * time.Millisecond)
+	// }
+	wg.Add(3)
+	go ledOUT("13")
+	go ledOUT("6")
+	go ledOUT("5")
+	go func() {
+		for i := range lights {
+			fmt.Println(i)
+		}
+	}()
+
+	wg.Wait()
+}
+
+func ledOUT(name string) {
+	for l := gpio.Low; ; l = !l {
+		gpioreg.ByName(name).Out(l)
+		time.Sleep(500 * time.Millisecond)
+		lights <- name
+	}
 }
